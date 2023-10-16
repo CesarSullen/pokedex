5405e5d8961a9b5e2db16571bd6d3a521d80a856
Cesar_Sullen <cesarsuarez2303@gmail.com>
2023-10-16 12:19:15 -0400

    Added the nav bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="nav">
+		<ul class="nav-list">
+			<li class="nav-item">
+				<button class="btn nav-btn" id="see-all">See All</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="normal">Normal</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="fire">Fire</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="water">Water</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="grass">Grass</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="electric">Electric</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="ice">Ice</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="fighting">Fighting</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="poison">Poison</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="ground">Ground</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="flying">Flying</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="psychic">Psychic</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="bug">Bug</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="rock">Rock</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="ghost">Ghost</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="dark">Dark</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="dragon">Dragon</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="steel">Steel</button>
+			</li>
+			<li class="nav-item">
+				<button class="btn nav-btn" id="fairy">Fairy</button>
+			</li>
+		</ul>
+	</div>
+</template>
